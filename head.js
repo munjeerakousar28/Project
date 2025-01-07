faf72dccfc8602fdfe55d5e0e323c96b76a816d7
munjeerakousar28 <113105375+munjeerakousar28@users.noreply.github.com>
2025-01-07 16:08:15 +0530

    Add files via upload

---
+++ head.js
@@ -0,0 +1,9 @@
+const toggleButton = document.querySelector('.toggle-button');
+const nav = document.querySelector('nav');
+
+toggleButton.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
+
+
+
